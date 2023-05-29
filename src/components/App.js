3b1cfe9ebaed1153450b211b59745e406a43932b
Anthony D'Andrea <anthonydandrea2@gmail.com>
2023-05-28 21:56:08 -0400

    Fix routing bug to display back arrow

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import '../styles/App.css';
 import React from 'react';
 import {Link, useLocation} from 'react-router-dom';
-import BackArrow from '../images/back-arrow.png';
 import SvgBackArrow from '../svgs/SvgBackArrow.js';
 import Footer from './Footer.js';
 
-const SUBROUTES = new Set(['/posts', '/resume', '/projects']);
+const TOP_LEVEL_ROUTES = ['/posts', '/resume', '/projects'];
+const SUBROUTES = new Set();
+TOP_LEVEL_ROUTES.forEach(route => {
+    // local development, url doesn't end with /,
+    // but prod does for some reason
+    SUBROUTES.add(route);
+    SUBROUTES.add(route + '/');
+});
 
 function App(props) {
 	const location = useLocation();
