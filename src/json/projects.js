879489a489c49fb159a15606febee150c84f7b0c
Anthony D'Andrea <anthonydandrea2@gmail.com>
2023-05-29 22:05:45 -0400

    Fixed image displays

--- src/json/projects.js
+++ src/json/projects.js
@@ -1,14 +1,17 @@
+import MetPro34Image from '../images/metpro34.png';
+import RudimentGeneratorImage from '../images/rudimentgenerator.png';
+
 export default [
     {
         "name": "MetPro34",
         "shortDescription": "Programmable metronome tool for music ensembles. Available for Web, iOS, and Android. Aquired by J.W. Pepper in Spring 2023.",
-        "imageUrl": process.env.PUBLIC_URL + 'metpro34.png',
+        "imageUrl": MetPro34Image,
         "redirectUrl": "https://metpro34.com/"
     },
     {
         "name": "RudimentGenerator.com",
         "shortDescription": "Probabilistic music generation for drummers with rules to guarantee playability.",
-        "imageUrl": process.env.PUBLIC_URL + 'rudimentgenerator.png',
+        "imageUrl": RudimentGeneratorImage,
         "redirectUrl": "https://rudimentgenerator.com/"
     }
 ]
